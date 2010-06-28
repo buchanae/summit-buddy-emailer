baae7e96f22cd5eabe1b77286622e6098ce12665
Alex Buchanan <abuchanan@mozilla.com>
2010-06-28 13:17:38 -0700

    email content and from-name

--- emailer.py
+++ emailer.py
@@ -12,13 +12,28 @@ if len(sys.argv) != 2:
 filename = sys.argv[1]
 reader = csv.reader(open(filename, "rb"))
 
-from_address = 'summit2010@mozilla.com'
+from_address = 'Mozilla Summit 2010 <summit2010@mozilla.com>'
 content = """
-Hi {recipient},
+Welcome {recipient} and {buddy} to the Mozilla Summit 2010 Buddy Program.   We've
+got some great fun lined up for you two in Whistler. (More on that in an
+upcoming newsletter.) But before we get there, we wanted to introduce you to
+each other. So, {recipient} meet {buddy}, {buddy} meet {recipient}.
 
-Your buddy is {buddy}
+While we think the Buddy Program is going to be a blast, you're not required to
+participate in all of the games we've got set up, but we would like you two to
+find each other at the welcome dinner and at least say hi and share a bit about
+what you do at Mozilla.
 
-Thanks,
+In order to make that easier, you might want to use this virtual introduction
+to let each other know where you're coming from and maybe share a photo or
+description of yourself so you'll be easier to find at the reception.
+
+We know you're going to have a great time at the Mozilla Summit 2010 and we
+hope you'll participate in all of the good times we're preparing for our 300
+buddy pairs.
+
+Looking forward to it,
+The Summit Buddy Team
 """
 
 s = smtplib.SMTP()
